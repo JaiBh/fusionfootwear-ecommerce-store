2d92ac7ccd2a6912c5e0832b63fa7ab00579926d
Jai Bhullar <81278507+JaiBh@users.noreply.github.com>
2025-06-04 01:26:26 +0100

    refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    screens: {
-      md: "700px",
+    extend: {
+      screens: {
+        md: "700px",
+      },
     },
   },
   variants: {
